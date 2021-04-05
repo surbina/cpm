b6ba0c20bdea06d9ad02d034d59c341e86f24220
Sebastian Urbina <surbina@mgmresorts.com>
2021-04-05 20:58:50 -0300

    reorder columns

--- generateFullList.mjs
+++ generateFullList.mjs
@@ -1,16 +1,23 @@
 import { table } from "./data.mjs";
 
 function printCSV(list) {
-  // console.log(list);
   console.log(
-    "Id,Duration,Earliest Start Time,Latest Start Time,Earliest End Time,Latest End Time,Predecessors,Holgura,Es Camino Critico?"
+    "Id,Duracion,Predecedores,Tiempo de inicio temprano,Tiempo de inicio tardio,Tiempo de finalizacion temprano,Tiempo de finalizacion tardio,Holgura,Es parte del camino critico?"
   );
   list.forEach(
     ({ id, duration, est, lst, eet, let: leet, predecessors, h }) => {
       console.log(
-        `${id},${duration},${est},${lst},${eet},${leet},${predecessors
-          .map(({ id }) => id)
-          .join(";")},${h},${h == 0}`
+        [
+          id,
+          duration,
+          predecessors.map(({ id }) => id).join(";"),
+          est,
+          lst,
+          eet,
+          leet,
+          h,
+          h == 0,
+        ].join(",")
       );
     }
   );
